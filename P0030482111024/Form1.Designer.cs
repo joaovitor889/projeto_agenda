134bc075cba6a482d63500d6bf3bcf761aab5b2f
JoaoReis <joaovitorreis@outlook.com>
2022-02-21 16:04:42 -0300

    Projeto de agenda virtual com banco de dados simples

---
+++ P0030482111024/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace P0030482111024
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstboxResultado = new System.Windows.Forms.ListBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstboxResultado
+            // 
+            this.lstboxResultado.FormattingEnabled = true;
+            this.lstboxResultado.ItemHeight = 15;
+            this.lstboxResultado.Location = new System.Drawing.Point(441, 12);
+            this.lstboxResultado.Name = "lstboxResultado";
+            this.lstboxResultado.Size = new System.Drawing.Size(347, 379);
+            this.lstboxResultado.TabIndex = 0;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnVerificar.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnVerificar.FlatAppearance.BorderSize = 3;
+            this.btnVerificar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnVerificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVerificar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnVerificar.Location = new System.Drawing.Point(12, 12);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(274, 117);
+            this.btnVerificar.TabIndex = 1;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = false;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lstboxResultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstboxResultado;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ P0030482111024/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P0030482111024
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double[,] matriz = new double[4, 4];
+            string auxiliar;
+            double[] total = new double[4];
+            double result =0;
+
+            for (var i = 0; i < 4; i++)
+            {
+                for (var j = 0; j < 4; j++)
+                {
+                    auxiliar = Interaction.InputBox("Digite o valor recebido da semana "+(j+1)+" do mês "+(i+1), "Entrada de dados");
+                    if (!Double.TryParse(auxiliar, out matriz[i, j]))
+                    {
+                        MessageBox.Show("Valor invalido!!!");
+                        j--;
+                    }
+                    else
+                    {
+                        lstboxResultado.Items.Add("Mês " + (i + 1) + "semana " + (j + 1) + ":" + matriz[i, j].ToString("C") + "\n");
+                        total[i] += matriz[i, j];
+                    }
+                }
+                lstboxResultado.Items.Add("Total mês " + (i + 1) + ":" + total[i].ToString("C") + "\n");
+                lstboxResultado.Items.Add("----------------------------------- \n");
+                result += total[i];
+            }
+            lstboxResultado.Items.Add("O total dos meses foi de : " + result.ToString("C"));
+
+        }
+    }
+}
